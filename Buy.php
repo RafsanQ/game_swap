4a06342c8b19a340c69df7059715d109879173dd
Rafsan <rafsan.quayes@gmail.com>
2023-06-02 00:11:45 +0600

    Basic search with text works now

--- Buy.php
+++ Buy.php
@@ -2,9 +2,18 @@
 
 include 'config.php';
 
-$sql = "SELECT * FROM games";
-$result = mysqli_query($conn, $sql);
+$result;
 
+// If the search bar is empty
+if(!isset($_GET['search_bar'])){
+  $sql = "SELECT * FROM games";
+  $result = mysqli_query($conn, $sql);
+}
+
+else{
+  $sql = "SELECT * FROM games WHERE name LIKE '%{$_GET['search_bar']}%'";
+  $result = mysqli_query($conn, $sql);
+}
 
 ?>
 
@@ -18,6 +27,7 @@ $result = mysqli_query($conn, $sql);
   <title>Product Catalog</title>
   <!-- Bootstrap CSS --><link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 </head>
+
 
 <body>
   <!-- Navigation Bar -->
@@ -54,10 +64,11 @@ $result = mysqli_query($conn, $sql);
 
 <!-- Search Bar -->
 <div class="d-flex flex-wrap mb-5">
-  <form class="form-inline ml-auto">
-    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+  <form class="form-inline ml-auto" method="get">
+    <input class="form-control mr-sm-2" type="search" name="search_bar" placeholder="Search" aria-label="Search">
     <button class="btn btn-outline-danger my-2 my-sm-0" type="submit">Search</button>
   </form>
+
   <!-- Sort Dropdown Menu -->
   <div class="dropdown ml-2">
     <button class="btn btn-secondary dropdown-toggle" type="button" id="sortMenu" data-toggle="dropdown"
@@ -145,6 +156,7 @@ $result = mysqli_query($conn, $sql);
   <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
-  
+
 </body>
+
 </html>
